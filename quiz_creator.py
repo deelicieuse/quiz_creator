5e7dd412663ae6cb56074e754765d89b8e57353e
deelicieuse <libatiqueangelique@gmail.com>
2025-04-13 17:42:04 +0800

    added create_widgets

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,18 +1,24 @@
 import tkinter as tk
-import tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox
 import json
 import os
 
 class QuizCreatorApp:
     def __init__(self, root_window):
         self.root_window = root_window
-        self.root_window.title("Quix Creator")
+        self.root_window.title("Quiz Creator")
 
         self.option_entries = {}
         self.option_imagepaths = {}
         self.option_image_labels = {}
         self.cirrect_answer_variable = tk.StringVar
         self.option_keys = ["a", "b", "c", "d"]
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        main_frame = tk.Frame(self.root_window, padx=10, pady=10)
+        main_frame.grid(row=0, column=0)
 
 if __name__ == "__main__":
     main_window = tk.Tk()
