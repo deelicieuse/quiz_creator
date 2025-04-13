d9f60d28b67a63bd0db55a215eb9bf0a359351e0
deelicieuse <libatiqueangelique@gmail.com>
2025-04-13 17:22:03 +0800

    pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,22 @@
+#
+# intialize setup
+#
+# show interface
+#     main screen
+#     prompt for question
+#     prompt for options (a-d)
+#         text field
+#         image button
+#     correct answer selection
+#     buttoms
+#         media button - select media and display
+#         clear image button
+#
+#
+# export
+#     save questions and options
+#     store data to text file
+#
+# clear input buttom
+#
+# start program
