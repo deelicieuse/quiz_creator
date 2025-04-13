080e1df212dd0b3334a7ef1cdbdd8468c8b08a0c
deelicieuse <libatiqueangelique@gmail.com>
2025-04-13 17:35:02 +0800

    define data storage and option keys

--- quiz_creator.py
+++ quiz_creator.py
@@ -8,6 +8,12 @@ class QuizCreatorApp:
         self.root_window = root_window
         self.root_window.title("Quix Creator")
 
+        self.option_entries = {}
+        self.option_imagepaths = {}
+        self.option_image_labels = {}
+        self.cirrect_answer_variable = tk.StringVar
+        self.option_keys = ["a", "b", "c", "d"]
+
 if __name__ == "__main__":
     main_window = tk.Tk()
     quiz_app = QuizCreatorApp(main_window)
