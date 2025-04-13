046edc5ec1717ea7a5cb33e12b49f575c12b5495
deelicieuse <libatiqueangelique@gmail.com>
2025-04-13 17:26:32 +0800

    imported tkinter and displayed basic window

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,22 +1,6 @@
-#
-# intialize setup
-#
-# show interface
-#     main screen
-#     prompt for question
-#     prompt for options (a-d)
-#         text field
-#         image button
-#     correct answer selection
-#     buttoms
-#         media button - select media and display
-#         clear image button
-#
-#
-# export
-#     save questions and options
-#     store data to text file
-#
-# clear input buttom
-#
-# start program
+import tkinter as tk
+
+if __name__ == "__main__":
+    main_window = tk.Tk()
+    main_window.title("Quiz Creator")
+    main_window.mainloop()
