0dc501560d1fbad9b46c4adf53dfa7a5dd5d4844
deelicieuse <libatiqueangelique@gmail.com>
2025-05-08 19:08:48 +0800

    added quiz ui frame

--- quiz_player.py
+++ quiz_player.py
@@ -19,23 +19,47 @@ class QuizPlayer:
     def build_ui(self):
 
         self.intro_frame = tk.Frame(self.root)
-        tk.Label(self.intro_frame,
-                 text="ARE YOU READY TO PLAY?",
-                 font=("Courier", 28, "bold")).pack(pady=20)
-        tk.Button(self.intro_frame,
-                  text="LET'S START",
-                  font=("Courier", 14),
-                  command=lambda:self.switch_frame(self.intro_frame)).pack()
+        tk.Label(
+            self.intro_frame,
+            text="ARE YOU READY TO PLAY?",
+            font=("Courier", 28, "bold")
+        ).pack(pady=20)
+        tk.Button(
+            self.intro_frame,
+            text="LET'S START",
+            font=("Courier", 14),
+            command=lambda:self.switch_frame(self.intro_frame)
+        ).pack()
         self.intro_frame.pack(fill="both", expand=True)
 
         self.file_selection_frame = tk.Frame(self.root)
-        tk.Label(self.file_selection_frame,
-                 text="Select Quiz JSON File",
-                 font=("Courier", 14)).pack(pady=10)
-        tk.Button(self.file_selection_frame,
-                  text="Browse...",
-                  font=("Courier", 12),
-                  command=self.choose_file).pack()
+        tk.Label(
+            self.file_selection_frame,
+            text="Select Quiz JSON File",
+            font=("Courier", 14)
+        ).pack(pady=10)
+        tk.Button(
+            self.file_selection_frame,
+            text="Browse...",
+            font=("Courier", 12),
+            command=self.choose_file
+        ).pack()
+
+        self.quiz_frame = tk.Frame(self.root, padx=20, pady=20)
+        self.questions_label = tk.Label(self.quiz_frame,
+                                        wraplength=500,
+                                        font=("Courier", 12)
+                                        )
+        self.questions_label.pack(pady=10)
+        self.answer_buttons = {}
+        for key in "abcd":
+            btn = tk.Button(
+            self.quiz_frame,
+            width=40,
+            font=("Courier", 12)
+            )
+            btn.pack(pady=3)
+            self.answer_buttons[key] = btn
 
     def choose_file(self):
         fp = filedialog.askopenfilename(filetypes=[("JSON", "*.json"), ("All", "*.*")])
