47edd4b6040fb79e6b6762db53f2fe0d22e7d75f
deelicieuse <libatiqueangelique@gmail.com>
2025-05-08 18:50:07 +0800

    added file selection frame

--- quiz_player.py
+++ quiz_player.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from struct import pack_into
+from traceback import format_tb
+
 
 class QuizPlayer:
     def __init__(self):
@@ -13,13 +16,24 @@ class QuizPlayer:
 
 
     def build_ui(self):
+
         self.intro_frame = tk.Frame(self.root)
-        tk.Label(self.intro_frame, text="ARE YOU READY TO PLAY?",
+        tk.Label(self.intro_frame,
+                 text="ARE YOU READY TO PLAY?",
                  font=("Courier", 28, "bold")).pack(pady=20)
-        tk.Button(self.intro_frame, text="LET'S START",
+        tk.Button(self.intro_frame,
+                  text="LET'S START",
                   font=("Courier", 14),
                   command=lambda: self.switch_frame(self.intro_frame)).pack()
         self.intro_frame.pack(fill="both", expand=True)
+
+        self.file_selection_frame = tk.Frame(self.root)
+        tk.Label(self.file_selection_frame,
+                 text="Select Quiz JSON File",
+                 font=("Courier", 14)).pack(pady=10)
+        tk.Button(self.file_selection_frame,
+                  text="Browse...",
+                  font=("Courier", 12)).pack()
 
     def run(self):
         self.root.mainloop()
