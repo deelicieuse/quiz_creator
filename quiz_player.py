d5baff260021f4b30e12fde0e2f216464cba987d
deelicieuse <libatiqueangelique@gmail.com>
2025-05-08 18:44:23 +0800

    add switch-frame logic

--- quiz_player.py
+++ quiz_player.py
@@ -6,10 +6,19 @@ class QuizPlayer:
         self.root.title("Quiz Player")
         self.build_ui()
 
+    def switch_frame(self, frame):
+        for ch in self.root.winfo_ch():
+            ch.pack_forget()
+        frame.pack(fill="both", expand=True)
+
+
     def build_ui(self):
         self.intro_frame = tk.Frame(self.root)
-        tk.Label(self.intro_frame, text="ARE YOU READY TO PLAY?", font=("Courier", 28, "bold")).pack(pady=20)
-        tk.Button(self.intro_frame, text="LET'S START", font=("Courier", 14)).pack()
+        tk.Label(self.intro_frame, text="ARE YOU READY TO PLAY?",
+                 font=("Courier", 28, "bold")).pack(pady=20)
+        tk.Button(self.intro_frame, text="LET'S START",
+                  font=("Courier", 14),
+                  command=lambda: self.switch_frame(self.intro_frame)).pack()
         self.intro_frame.pack(fill="both", expand=True)
 
     def run(self):
